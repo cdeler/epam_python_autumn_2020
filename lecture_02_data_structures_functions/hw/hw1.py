c0643f0a16735da5d9d403dca8cfb93d161e19e9
andrei_shulaev <andrei_shulaev@epam.com>
2020-10-29 21:51:09 +0300

    add slides for lecture 02

---
+++ lecture_02_data_structures_functions/hw/hw1.py
@@ -0,0 +1,29 @@
+"""
+Given a file containing text. Complete using only default collections:
+    1) Find 10 longest words consisting from largest amount of unique symbols
+    2) Find rarest symbol for document
+    3) Count every punctuation char
+    4) Count every non ascii char
+    5) Find most common non ascii char for document
+"""
+from typing import List
+
+
+def get_longest_diverse_words(file_path: str) -> List[str]:
+    ...
+
+
+def get_rarest_char(file_path: str) -> str:
+    ...
+
+
+def count_punctuation_chars(file_path: str) -> int:
+    ...
+
+
+def count_non_ascii_chars(file_path: str) -> int:
+    ...
+
+
+def get_most_common_non_ascii_char(file_path: str) -> str:
+    ...
---
+++ lecture_02_data_structures_functions/hw/hw4.py
@@ -0,0 +1,25 @@
+"""
+Write a function that accepts another function as an argument. Then it
+should return such a function, so the every call to initial one
+should be cached.
+
+
+def func(a, b):
+    return (a ** b) ** 2
+
+
+cache_func = cache(func)
+
+some = 100, 200
+
+val_1 = cache_func(*some)
+val_2 = cache_func(*some)
+
+assert val_1 is val_2
+
+"""
+from collections import Callable
+
+
+def cache(func: Callable) -> Callable:
+    ...
