456f576fe0330c3df8fcf2f9774ff7122553c3ea
cdeler <serj.krotov@gmail.com>
2020-10-29 19:45:49 +0300

    Add lecture 01 (#1)
    * Added sample project
    
    * Added homework
    
    * Added slideshow
    
    * pin black version
    
    * Updated slideshow
    
    Co-authored-by: cdeler <sergei_krotov@epam.com>

---
+++ lecture_01/hw/task04.py
@@ -0,0 +1,13 @@
+"""
+Classic task, a kind of walnut for you
+
+Given four lists A, B, C, D of integer values,
+    compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
+"""
+from typing import List
+
+
+def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+    ...
