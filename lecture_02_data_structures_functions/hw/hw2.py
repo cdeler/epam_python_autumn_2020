e49e58318861655eb1a355e40f76a55bb80680c9
Andrey Shulaev <35524482+andrejshulaev@users.noreply.github.com>
2020-11-02 21:18:01 +0300

    add lecture 02 slides (#3)
    * add slides for lecture 02
    
    * Update lecture_02_data_structures_functions/hw/hw2.py
    
    * Minor updates
    
    Co-authored-by: andrei_shulaev <andrei_shulaev@epam.com>
    Co-authored-by: cdeler <serj.krotov@gmail.com>

---
+++ lecture_02_data_structures_functions/hw/hw2.py
@@ -0,0 +1,24 @@
+"""
+Given an array of size n, find the most common and the least common elements.
+The most common element is the element that appears more than n // 2 times.
+The least common element is the element that appears fewer than other.
+
+You may assume that the array is non-empty and the most common element
+always exist in the array.
+
+Example 1:
+
+Input: [3,2,3]
+Output: 3, 2
+
+Example 2:
+
+Input: [2,2,1,1,1,2,2]
+Output: 2, 1
+
+"""
+from typing import List, Tuple
+
+
+def major_and_minor_elem(inp: List) -> Tuple[int, int]:
+    ...
