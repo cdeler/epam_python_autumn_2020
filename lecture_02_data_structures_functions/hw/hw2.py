4ac2388f41191048e8892553d800dcf03c849b77
cdeler <serj.krotov@gmail.com>
2020-11-02 10:54:51 +0300

    Update lecture_02_data_structures_functions/hw/hw2.py

--- lecture_02_data_structures_functions/hw/hw2.py
+++ lecture_02_data_structures_functions/hw/hw2.py
@@ -1,7 +1,7 @@
 """
 Given an array of size n, find the most common and the least common elements.
 The most common element is the element that appears more than n // 2 times.
-The least element is the element that appears fewer than other.
+The least common element is the element that appears fewer than other.
 
 You may assume that the array is non-empty and the most common element
 always exist in the array.
