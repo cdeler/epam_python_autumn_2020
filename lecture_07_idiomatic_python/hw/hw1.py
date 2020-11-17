a316b20606bf37c7741e2dd8ea14f0a09148def2
Roman Tsaregorodtsev <Roman_Tsaregorodtsev@epam.com>
2020-11-17 23:38:02 +0300

    add idiomatic python lection
    updates
    
    add homeworks
    improve presentation

---
+++ lecture_07_idiomatic_python/hw/hw1.py
@@ -0,0 +1,36 @@
+"""
+Given a dictionary (tree), that can contains multiple nested structures.
+Write a function, that takes element and finds the number of occurrences
+of this element in the tree.
+
+Tree can only contains basic structures like:
+    str, list, tuple, dict, set, int, bool
+"""
+from typing import Any
+
+
+# Example tree:
+example_tree = {
+    "first": ["RED", "BLUE"],
+    "second": {
+        "simple_key": ["simple", "list", "of", "RED", "valued"],
+    },
+    "third": {
+        "abc": "BLUE",
+        "jhl": "RED",
+        "complex_key": {
+            "key1": "value1",
+            "key2": "RED",
+            "key3": ["a", "lot", "of", "values", {"nested_key": "RED"}],
+        }
+     },
+    "fourth": "RED",
+}
+
+
+def find_occurrences(tree: dict, element: Any) -> int:
+    ...
+
+
+if __name__ == '__main__':
+    print(find_occurrences(example_tree, "RED"))  # 6
---
+++ lecture_07_idiomatic_python/hw/hw2.py
@@ -0,0 +1,24 @@
+"""
+Given two strings. Return if they are equal when both are typed into
+empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Examples:
+    Input: s = "ab#c", t = "ad#c"
+    Output: True
+    # Both s and t become "ac".
+
+    Input: s = "a##c", t = "#a#c"
+    Output: True
+    Explanation: Both s and t become "c".
+
+    Input: a = "a#c", t = "b"
+    Output: False
+    Explanation: s becomes "c" while t becomes "b".
+
+"""
+
+
+def backspace_compare(first: str, second: str):
+    ...
