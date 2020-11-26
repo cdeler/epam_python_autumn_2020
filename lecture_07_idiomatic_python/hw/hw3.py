bce26795fa9a266e0d5664858e974ff1abec674c
Andrey <Cjkjvfnby@gmail.com>
2020-11-26 20:42:09 +0300

    add idiomatic python lection (#11)
    add homeworks
    improve presentation
    
    Co-authored-by: Roman Tsaregorodtsev <Roman_Tsaregorodtsev@epam.com>

---
+++ lecture_07_idiomatic_python/hw/hw3.py
@@ -0,0 +1,26 @@
+"""
+Given a Tic-Tac-Toe 3x3 board (can be unfinished).
+Write a function that checks if the are some winners.
+If there is "x" winner, function should return "x wins!"
+If there is "o" winner, function should return "o wins!"
+If there is a draw, function should return "draw!"
+If board is unfinished, function should return "unfinished!"
+
+Example:
+    [[-, -, o],
+     [-, x, o],
+     [x, o, x]]
+    Return value should be "unfinished"
+
+    [[-, -, o],
+     [-, o, o],
+     [x, x, x]]
+
+     Return value should be "x wins!"
+
+"""
+from typing import List
+
+
+def tic_tac_toe_checker(board: List[List]) -> str:
+    ...
