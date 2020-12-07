91dac356b09650296e5fddeaf1ab1876fb757e54
Roman Tsaregorodtsev <Roman_Tsaregorodtsev@epam.com>
2020-12-07 22:03:44 +0300

    add lecture 09

---
+++ lecture_09_iterators/hw/hw3.py
@@ -0,0 +1,20 @@
+"""
+Write a function that takes directory path, a file extension and an optional tokenizer.
+It will count lines in all files with that extension if there are no tokenizer.
+If a the tokenizer is not none, it will count tokens.
+
+For dir with two files from hw1.py:
+>>> universal_file_counter(test_dir, "txt")
+6
+>>> universal_file_counter(test_dir, "txt", str.split)
+6
+
+"""
+from pathlib import Path
+from typing import Optional, Callable
+
+
+def universal_file_counter(
+    dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None
+) -> int:
+    pass
