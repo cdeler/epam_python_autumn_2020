97354470055563a3e01a7c1fb3615480250bcdfb
AlekseiAfanasev <69589735+AlekseiAfanasev@users.noreply.github.com>
2020-12-18 12:45:14 +0300

    Add lecture 11 (#15)

---
+++ lecture_11_advanced_topics/hw/hw1.py
@@ -0,0 +1,35 @@
+"""
+Vasya implemented nonoptimal Enum classes.
+Remove duplications in variables declarations using metaclasses.
+
+from enum import Enum
+
+
+class ColorsEnum(Enum):
+    RED = "RED"
+    BLUE = "BLUE"
+    ORANGE = "ORANGE"
+    BLACK = "BLACK"
+
+
+class SizesEnum(Enum):
+    XL = "XL"
+    L = "L"
+    M = "M"
+    S = "S"
+    XS = "XS"
+
+
+Should become:
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    __keys = ("XL", "L", "M", "S", "XS")
+
+
+assert ColorsEnum.RED == "RED"
+assert SizesEnum.XL == "XL"
+"""
---
+++ lecture_11_advanced_topics/hw/hw2.py
@@ -0,0 +1,30 @@
+"""
+You are given the following code:
+
+class Order:
+    morning_discount = 0.25
+
+    def __init__(self, price):
+        self.price = price
+
+    def final_price(self):
+        return self.price - self.price * self.morning_discount
+
+Make it possible to use different discount programs.
+Hint: use strategy behavioural OOP pattern.
+https://refactoring.guru/design-patterns/strategy
+
+Example of the result call:
+
+def morning_discount(order):
+    ...
+
+def elder_discount(order):
+    ...
+
+order_1 = Order(100, morning_discount)
+assert order_1.final_price() == 50
+
+order_2 = Order(100, elder_discount)
+assert order_1.final_price() == 10
+"""
