700a8c120dde593480e125010f378467a7de8808
Andrey <Cjkjvfnby@gmail.com>
2021-03-10 18:28:14 +0300

    Remove doctest from the task description

--- lecture_04_test/hw/task_3_get_print_output.py
+++ lecture_04_test/hw/task_3_get_print_output.py
@@ -3,12 +3,12 @@ Write a function that will receive a string and write it to stderr
 if line starts with "error" and to the stdout otherwise.
 
 
->>> my_precious_logger("error: file not found")
+my_precious_logger("error: file not found")
 # stderr
 'error: file not found'
 
 
->>> my_precious_logger("OK")
+my_precious_logger("OK")
 # stdout
 'OK'
 
@@ -20,8 +20,6 @@ Definition of done:
 You will learn:
  - how to write to stderr
  - how to test output to the stderr and stdout
-
-
 """
 
 
