43cdcb6e7152e7629343f536596058001064b4f1
Natalia <65091469+suppnova@users.noreply.github.com>
2021-08-06 12:37:17 +0300

    Update hw2.py (#26)
    1) if final_price is self.price - self.price * self.morning_discount then = 100 - 100*0.25 = 75
    2) second assert presumably refers to second order

--- lecture_11_advanced_topics/hw/hw2.py
+++ lecture_11_advanced_topics/hw/hw2.py
@@ -23,8 +23,8 @@ def elder_discount(order):
     ...
 
 order_1 = Order(100, morning_discount)
-assert order_1.final_price() == 50
+assert order_1.final_price() == 75
 
 order_2 = Order(100, elder_discount)
-assert order_1.final_price() == 10
+assert order_2.final_price() == 10
 """
